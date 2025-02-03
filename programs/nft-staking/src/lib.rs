8d5a5e107807b3f6562111dbbf0199cf2c009996
xavierScript <david111francisco@gmail.com>
2025-02-03 18:54:40 +0100

    buffed up codebase

--- programs/nft-staking/src/lib.rs
+++ programs/nft-staking/src/lib.rs
@@ -6,11 +6,23 @@ declare_id!("5ZsUk5GEfARGJVjcR5UpwW9DTK4fMAx7Gsw1vAN9JmCX");
 pub mod nft_staking {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize_config(ctx: Context<InitializeConfig>, points_per_stake: u8, max_stake: u8, freeze_period: u32) -> Result<()> {
+        ctx.accounts.initialize_config(points_per_stake, max_stake, freeze_period, &ctx.bumps)
+    }
+
+    pub fn initialize_user(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.initialize_user(&ctx.bumps)
+    }
+
+    pub fn stake(ctx: Context<Stake>) -> Result<()> {
+        ctx.accounts.stake(&ctx.bumps)
+    }
+
+    pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
+        ctx.accounts.unstake()
+    }
+
+    pub fn claim(ctx: Context<Claim>) -> Result<()> {
+        ctx.accounts.claim()
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
